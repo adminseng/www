acb2fdbeec7bfab332ebf528980d0289f7f8cc7d
yeejoe <yeejoe94@gmail.com>
2015-09-01 00:04:05 +0800

    Search Site modification done

--- js/searchSite.js
+++ js/searchSite.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+    $("#search-result").html("<div align='center'><img alt='Loading' src='/img/sales/db-load.gif'/></div>");
     //retrieve the value inside $_GET
     var $_GET = {};
     document.location.search.replace(/\??(?:([^=]+)=([^&]*)&?)/g, function () {
@@ -8,16 +9,22 @@ $(document).ready(function () {
         $_GET[decode(arguments[1])] = decode(arguments[2]);
     });
     var keyword = $_GET["q"];
+
     $.getJSON("result.txt", function (data) {
+        $("#search-result").html("");
         $.each(data, function () {
             if (this.title.toLowerCase().indexOf(keyword.toLowerCase()) > -1) {
                 buildHTMLScript(this);
             }
         });
+        if ($("#search-result").children().length <= 0) {
+            $("#search-result").append("<div class='alert alert-error' align='center'><strong>No Result Found, Please Try Again!</strong></div>");
+        }
     });
 });
 
 $("#form-search").submit(function () {
+    $("#search-result").html("<div align='center'><img alt='Loading' src='/img/sales/db-load.gif'/></div>");
     //retrieve the value inside $_GET
     var $_GET = {};
     document.location.search.replace(/\??(?:([^=]+)=([^&]*)&?)/g, function () {
@@ -29,13 +36,16 @@ $("#form-search").submit(function () {
     var keyword = $_GET["q"];
     //load a local text file as JSON object into variable "data"
     $.getJSON("result.txt", function (data) {
+        $("#search-result").html("");
         $.each(data, function () {
             //compare the keyword with title of data object
             if (this.title.toLowerCase().indexOf(keyword.toLowerCase()) > -1) {
                 buildHTMLScript(this);
-
             }
         });
+        if ($("#search-result").children().length <= 0) {
+            $("#search-result").append("<div class='alert alert-error' align='center'><strong>No Result Found, Please Try Again!</strong></div>");
+        }
     });
 });
 
