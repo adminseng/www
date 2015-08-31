07e4358873b597e76d7451714fedf3fa87946621
yeejoe <yeejoe94@gmail.com>
2015-08-31 09:42:40 +0800

    Price List added instead of calculator

---
+++ js/calculator.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    $("#calculatorBtn").click(function () {
+        $.fancybox({
+            //'href': 'http://www.emailmeform.com/builder/form/1nex1Gw0Pf6q6fCEmKfWrhc',
+            'href': '/img/pricelist.jpg'
+            
+        });
+    });
+});
